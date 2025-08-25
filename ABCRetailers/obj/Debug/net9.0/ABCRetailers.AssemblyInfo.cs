30c7ef10bf9fd7d78505d948146922ffef82573c
ST10395938 <130601150+Ochwo-Anthony@users.noreply.github.com>
2025-08-25 23:25:12 +0200

    Added Azure Storage NuGet packages for Tables, Blobs, Queues, and Files
    - Installed Azure.Data.Tables (v12.11.0) for customer profiles and product information.
    - Installed Azure.Storage.Blobs (v12.25.0) for image and multimedia storage.
    - Installed Azure.Storage.Files.Shares (v12.23.0) for contract storage.
    - Installed Azure.Storage.Queues (v12.23.0) for order and inventory processing.
    - Verified all packages appear under Dependencies > Packages.
    - Checked for Microsoft.VisualStudio.Web.CodeGeneration.Design (v9.0.0).

--- ABCRetailers/obj/Debug/net9.0/ABCRetailers.AssemblyInfo.cs
+++ ABCRetailers/obj/Debug/net9.0/ABCRetailers.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ABCRetailers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a13343180ea91258024d402fc234aa325e8e28e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0320cf51364ecdde5c585dbd2661cddc799c84d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ABCRetailers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ABCRetailers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
